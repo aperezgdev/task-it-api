ed57e35953ad4347e207d3e896b8c8fb63a66a7a
Alex Perez Garcia <aperezgdev@gmail.com>
2025-01-16 22:38:16 +0100

    fix: change find using optional

--- internal/domain/repository/user_repository.go
+++ internal/domain/repository/user_repository.go
@@ -4,8 +4,11 @@ import (
 	"context"
 
 	"github.com/aperezgdev/task-it-api/internal/domain/model"
+	valueobject "github.com/aperezgdev/task-it-api/internal/domain/value_object"
+	"github.com/aperezgdev/task-it-api/pkg"
 )
 
 type UserRepository interface {
+	Find(ctx context.Context, userId valueobject.Id) (pkg.Optional[model.User], error)
 	Save(ctx context.Context, user model.User) error
 }
