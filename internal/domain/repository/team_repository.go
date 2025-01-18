b3213beb6df2a67a77bafc61aaed63155975c84a
Alex Perez Garcia <aperezgdev@gmail.com>
2025-01-18 22:37:32 +0100

    feat: add team finder

--- internal/domain/repository/team_repository.go
+++ internal/domain/repository/team_repository.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/aperezgdev/task-it-api/internal/domain/model"
 	valueobject "github.com/aperezgdev/task-it-api/internal/domain/value_object"
+	"github.com/aperezgdev/task-it-api/pkg"
 	"github.com/stretchr/testify/mock"
 )
 
 type TeamRepository interface {
+	Find(ctx context.Context, idTeam valueobject.Id) (pkg.Optional[model.Team], error)
 	Save(ctx context.Context, team model.Team) error
 	Delete(ctx context.Context, idTeam valueobject.Id) error
 	Update(ctx context.Context, team model.Team) error
@@ -16,6 +18,11 @@ type TeamRepository interface {
 
 type MockTeamRepository struct {
 	mock.Mock
+}
+
+func (m *MockTeamRepository) Find(ctx context.Context, idTeam valueobject.Id) (pkg.Optional[model.Team], error) {
+	args := m.Called(ctx, idTeam)
+	return args.Get(0).(pkg.Optional[model.Team]), args.Error(1)
 }
 
 func (m *MockTeamRepository) Save(ctx context.Context, team model.Team) error {
