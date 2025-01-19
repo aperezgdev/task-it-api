6797cd71705640de491feee337f3418e16e31877
Alex Perez Garcia <aperezgdev@gmail.com>
2025-01-19 21:56:22 +0100

    feat: add Find methods

--- internal/domain/repository/status_repository.go
+++ internal/domain/repository/status_repository.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/aperezgdev/task-it-api/internal/domain/model"
 	valueobject "github.com/aperezgdev/task-it-api/internal/domain/value_object"
+	"github.com/aperezgdev/task-it-api/pkg"
 	"github.com/stretchr/testify/mock"
 )
 
 type StatusRepository interface {
+	Find(ctx context.Context, statusId valueobject.Id) (pkg.Optional[model.Status], error)
 	Save(ctx context.Context, status model.Status) error
 	Delete(ctx context.Context, statusId valueobject.Id) error
 	Update(ctx context.Context, status model.Status) error
@@ -16,6 +18,11 @@ type StatusRepository interface {
 
 type MockStatusRepository struct {
 	mock.Mock
+}
+
+func (m *MockStatusRepository) Find(ctx context.Context, statusId valueobject.Id) (pkg.Optional[model.Status], error) {
+	args := m.Called(ctx, statusId)
+	return args.Get(0).(pkg.Optional[model.Status]), args.Error(1)
 }
 
 func (m *MockStatusRepository) Save(ctx context.Context, status model.Status) error {
--- internal/domain/repository/task_repository.go
+++ internal/domain/repository/task_repository.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/aperezgdev/task-it-api/internal/domain/model"
 	valueobject "github.com/aperezgdev/task-it-api/internal/domain/value_object"
+	"github.com/aperezgdev/task-it-api/pkg"
 	"github.com/stretchr/testify/mock"
 )
 
 type TaskRepository interface {
+	Find(ctx context.Context, taskId valueobject.Id) (pkg.Optional[model.Task], error)
 	Save(ctx context.Context, task model.Task) error
 	Delete(ctx context.Context, taskId valueobject.Id) error
 	Update(ctx context.Context, task model.Task) error
@@ -16,6 +18,11 @@ type TaskRepository interface {
 
 type MockTaskRepository struct {
 	mock.Mock
+}
+
+func (m *MockTaskRepository) Find(ctx context.Context, taskId valueobject.Id) (pkg.Optional[model.Task], error) {
+	args := m.Called(ctx, taskId)
+	return args.Get(0).(pkg.Optional[model.Task]), args.Error(1)
 }
 
 func (m *MockTaskRepository) Save(ctx context.Context, task model.Task) error {
