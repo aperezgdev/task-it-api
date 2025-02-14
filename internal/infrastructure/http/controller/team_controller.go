305b40138a9829abf3beb80f4c22e692df871f8c
Alex Perez Garcia <aperezgdev@gmail.com>
2025-02-14 19:18:52 +0100

    feat: add team controller add and remover member

--- internal/infrastructure/http/controller/team_controller.go
+++ internal/infrastructure/http/controller/team_controller.go
@@ -21,6 +21,10 @@ type teamPostRequest struct {
 	Owner string `json:"owner"`
 }
 
+type teamPostMemberRequest struct {
+	Id string `json:"id"`
+}
+
 func NewTeamController(logger slog.Logger, creator team.TeamCreator, removeMember team.RemoverMember, addMember team.TeamAddMember) TeamController {
 	return TeamController{logger, creator, removeMember, addMember}
 }
@@ -46,4 +50,45 @@ func (tc *TeamController) PostTeam(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (tc *TeamController) PostMemberController(w http.ResponseWriter, r http.Request) {
+	var memberRequest teamPostMemberRequest
+	var teamId = r.PathValue("teamId")
+	err := json.NewDecoder(r.Body).Decode(&memberRequest)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	errCreator := tc.addMember.Run(
+		r.Context(),
+		teamId,
+		memberRequest.Id,
+	)
+	
+	if errCreator != nil {
+		writeError(w, errCreator)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (tc *TeamController) DeleteMemberController(w http.ResponseWriter, r http.Request) {
+	var teamId = r.PathValue("teamId")
+	var memberId = r.PathValue("memberId")
+
+	errCreator := tc.removeMember.Run(
+		r.Context(),
+		teamId,
+		memberId,
+	)
+	
+	if errCreator != nil {
+		writeError(w, errCreator)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
